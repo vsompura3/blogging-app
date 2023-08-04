14380365a20342f35f06096d7fa324761b7b12ce
vsompura3 <vsompura3@gmail.com>
2023-08-04 10:44:49 +0530

    🎨 : improved the structure of auth response

--- server/src/routes/api/v1/auth/auth.route.js
+++ server/src/routes/api/v1/auth/auth.route.js
@@ -2,7 +2,7 @@ import express from 'express'
 const authRouter = express.Router()
 
 authRouter.post('/login', (req, res) => {
-  res.send('logged in')
+  res.json({ message: 'logged in' })
 })
 
 export default authRouter
