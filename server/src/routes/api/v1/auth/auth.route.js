e951953787ce7eccc94dc35fd555d6aa1c55086b
vsompura3 <vsompura3@gmail.com>
2023-08-04 10:45:48 +0530

    ✨ : Added new routes for auth

--- server/src/routes/api/v1/auth/auth.route.js
+++ server/src/routes/api/v1/auth/auth.route.js
@@ -5,4 +5,12 @@ authRouter.post('/login', (req, res) => {
   res.json({ message: 'logged in' })
 })
 
+authRouter.post('/signup', (req, res) => {
+  res.json({ message: 'signed up' })
+})
+
+authRouter.get('/logout', (req, res) => {
+  res.json({ message: 'logged out' })
+})
+
 export default authRouter
